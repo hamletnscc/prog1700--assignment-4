3a50bebef45843aee29497ce762cb661ff3464a0
hamletnscc <113032060+hamletnscc@users.noreply.github.com>
2022-11-15 15:16:16 -0400

    Update q3.js

--- q3.js
+++ q3.js
@@ -1,1 +1,16 @@
+"use strict";
+/*
+Author:
+Assignment #:
+Description:
+*/
 
+function main() // <-- Don't change this line!
+{ 
+ 
+}
+// Do not change any of the code below!
+if (require.main === module)
+{
+    main();
+}
